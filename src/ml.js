1cf6e0cd7ec247db57905aad4260070a113b3203
Vinith-arun <vinith.p@tringapps.com>
2021-11-20 18:20:48 +0530

    first commit

--- src/ml.js
+++ src/ml.js
@@ -1,36 +1,39 @@
-import React,{useState} from 'react';
+import React,{useEffect} from 'react';
 import axios from 'axios';
+import './ml.css';
+  
 
-[data,setData]=useState([]);
-export default function ml(props) {
-
-
-    axios.get('http://localhost:9000/'+props.name)
-    .then(resp => {
-        setData(resp.data);
-        console.log(resp.data);
-        setCount(count+1);console.log(count);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-function movieDetail(prop){
-    console.log(prop.target.id);
-}
+function Mlf(props) {
+    const [data, setData] = React.useState([{}]);
+    function fetchMovieList(){
+        console.log('clicked')
+        axios.get(`http://localhost:9000/${props.name}`)
+        .then(resp => {
+            setData(resp.data);
+            console.log('dat dat dat')
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    
+    function movieDetail(p){
+        console.log(p.target.id);
+    }
     return (
         <div>
-            <h3>{props.name}</h3>
-            <div id="list">
-                {data.map(()=>{
-                    <div id={data.id} onClick={movieDetail}>
-                        <img src={data.img} alt="img"></img>
-                        <p>{data.name}</p> <span>{data.price}</span>
-                    </div>
-                })}
+            <h3 onClick={fetchMovieList}>{props.listname}</h3>
+            <div id="list" >
+                {data.map(ele=>(
+                    <div className="seperate" key={ele.id} id={ele.id} onClick={movieDetail}>
+                        {/* <img src={data.img} alt="img"></img> */}
+                        <p >{ele.name}</p> <span>Rs.{ele.price}</span>
+                    </div>)
+                )}
+                <div id="more">More..</div>
             </div>
         </div>
-    )
+    )   
 }
-
-
+console.log("after return");
+export default Mlf;
