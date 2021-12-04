4b82effb904d50f1558aeeaf2f04ac68afce1296
Vinith-arun <vinith.p@tringapps.com>
2021-12-04 16:51:26 +0530

    optimized api

--- src/home.js
+++ src/home.js
@@ -1,10 +1,10 @@
 import React,{useState,useEffect} from 'react'
-import axios from 'axios'
-const all =require('./store/store');
+import {  fetchMovieList } from './controllers/apis';
+// const all =require('./store/store');
 function movieBody(props){
     return(
-        <div className="movie" style={{display:'flex',flexDirection:'column',padding:'10px',width:'20vw'}}>
-            <img className="movieImg" alt="img"></img>
+        <div key={props.id} className="movie" style={{display:'flex',flexDirection:'column',padding:'10px',width:'15vw',alignItems:"center"}}>
+            <img className="movieImg" alt="img" src={props.img} style={{width:"15vw",height:"auto"}}></img>
             <div className="movieBody" style={{display:'flex',flexDirection:'row'}}>
                 <p className="movieName">{props.moviename+" "} </p>
                 <p className="movieYear">{props.year}</p>
@@ -13,9 +13,8 @@ function movieBody(props){
     )
 }
 function listContainer(props){
-    
     return(
-        <div id="list" style={{display:'flex',flexDirection:'column'}}>
+        <div id="list" key={props.id} style={{display:'flex',flexDirection:'column'}}>
             <div className="title_and_button" style={{display:'flex',flexDirection:'row'}}>
                 <h3 className="listTitle">{props.listTitle}</h3>
                 <button className="moreButton" style={{float:'right'}}>More</button>
@@ -29,95 +28,46 @@ function listContainer(props){
         </div>
 )
 }
-
-export default  function Home() {
-    
-const[latest_and_trending_home,setlatest_and_trending_home]=useState([])
-const[action_home,setaction_home]=useState([])
-const[kids_home,setkids_home]=useState([])
-const[thriller_home,setthriller_home]=useState([])
-const[crime_home,setcrime_home]=useState([])
-const[comedy_home,setcomedy_home]=useState([])
-const[romantic_home,setromantic_home]=useState([])
-const listjson=[{"listTitle":"Latest and trending","listobject":latest_and_trending_home},
-{"listTitle":"Action","listobject":action_home},
-{"listTitle":"Kids","listobject":kids_home},
-{"listTitle":"Thriller","listobject":thriller_home},
-{"listTitle":"Crime","listobject":crime_home},
-{"listTitle":"Comedy","listobject":comedy_home},
-{"listTitle":"Romantic","listobject":romantic_home}]
-
-console.log("Before UseEffect")
+export default  function Home() {   
+const fetching={ moviename: "Fetching", year: "Fetching",id:'1'}
+const notFetched=[fetching,fetching,fetching,fetching,fetching]
+const[latest_and_trending_home,setlatest_and_trending_home]=useState([notFetched])
+const[action_home,setaction_home]=useState([notFetched])
+const[kids_home,setkids_home]=useState([notFetched])
+const[thriller_home,setthriller_home]=useState([notFetched])
+const[crime_home,setcrime_home]=useState([notFetched])
+const[comedy_home,setcomedy_home]=useState([notFetched])
+const[romantic_home,setromantic_home]=useState([notFetched])
+const listjson=[
+{"listTitle":"Latest and trending","listobject":latest_and_trending_home,"id":1},
+{"listTitle":"Action","listobject":action_home,"id":2},
+{"listTitle":"Kids","listobject":kids_home,"id":3},
+{"listTitle":"Thriller","listobject":thriller_home,"id":4},
+{"listTitle":"Crime","listobject":crime_home,"id":5},
+{"listTitle":"Comedy","listobject":comedy_home,"id":6},
+{"listTitle":"Romantic","listobject":romantic_home,"id":7}]
 useEffect(()=>{
-    const notFetched=[{ moviename: "Fetching", year: "Fetching"},
-    { moviename: "Fetching", year: "Fetching"},
-    { moviename: "Fetching", year: "Fetching"},
-    { moviename: "Fetching", year: "Fetching"},
-    { moviename: "Fetching", year: "Fetching"}]
-  
-    axios.get(`http://localhost:9000/latest`)
-    .then(resp => {
-        setlatest_and_trending_home(resp.data)
-        console.log(resp.data)
-    })
-    .catch(function (error) {
-        setlatest_and_trending_home(notFetched)
-    console.log(error);
-    });
-
-    axios.get(`http://localhost:9000/action`)
-    .then(resp => {
-        setaction_home(resp.data)
-    })
-    .catch(function (error) {
-        setaction_home(notFetched)
-    console.log(error);
-    });
-
-    axios.get(`http://localhost:9000/kids`)
-    .then(resp => {
-        setkids_home(resp.data)
-    })
-    .catch(function (error) {
-    setkids_home(notFetched)
-    console.log(error);
-    });
-
-    axios.get(`http://localhost:9000/thriller`)
-    .then(resp => {
-        setthriller_home(resp.data)
-    })
-    .catch(function (error) {
-        setthriller_home(notFetched)
-    console.log(error);
-    });
-
-    axios.get(`http://localhost:9000/crime`)
-    .then(resp => {
-        setcrime_home(resp.data)
-    })
-    .catch(function (error) {
-        setcrime_home(notFetched)
-    console.log(error);
-    });
-
-    axios.get(`http://localhost:9000/comedy`)
-    .then(resp => {
-        setcomedy_home(resp.data)
-    })
-    .catch(function (error) {
-        setcomedy_home(notFetched)
-    console.log(error);
-    });
-
-    axios.get(`http://localhost:9000/romantic`)
-    .then(resp => {
-        setromantic_home(resp.data)
-    })
-    .catch(function (error) {
-        setromantic_home(notFetched)
-    console.log(error);
-    });
+    fetchMovieList('latest')
+    .then(resp =>setlatest_and_trending_home(resp.data))
+    .catch(e=>console.log(e));
+    fetchMovieList('action')
+    .then(resp => {setaction_home(resp.data)})
+    .catch(e=>{console.log(e)});
+    fetchMovieList('crime')
+    .then(resp => {setkids_home(resp.data)})
+    .catch(e=>{console.log(e)});
+    fetchMovieList('crime')
+    .then(resp => {setcrime_home(resp.data)})
+    .catch(e=>{console.log(e)});
+    fetchMovieList('thriller')
+    .then(resp => {setthriller_home(resp.data)})
+    .catch(e=>{console.log(e)});
+    fetchMovieList('comedy')
+    .then(resp => {setcomedy_home(resp.data)})
+    .catch(e=>{console.log(e)});
+    fetchMovieList('romantic')
+    .then(resp => {setromantic_home(resp.data)})
+    .catch(e=>{console.log(e)});   
 },[])
     return (
         <div id="homeContainer">
